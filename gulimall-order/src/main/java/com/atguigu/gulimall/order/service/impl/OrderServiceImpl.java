5d9fe4f269ce9170cbb8535288840633d0ca8a88
zqbinary <zqbinary@foxmail.com>
2023-05-06 14:07:13 +0800

    cart

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderServiceImpl.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderServiceImpl.java
@@ -1,16 +1,18 @@
 package com.atguigu.gulimall.order.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.Query;
-
 import com.atguigu.gulimall.order.dao.OrderDao;
 import com.atguigu.gulimall.order.entity.OrderEntity;
 import com.atguigu.gulimall.order.service.OrderService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 
 @Service("orderService")
@@ -24,6 +26,24 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
         );
 
         return new PageUtils(page);
+    }
+
+    //import org.springframework.transaction.annotation.Propagation;
+    //import org.springframework.transaction.annotation.Transactional;
+    //a事务
+    @Transactional(timeout = 30)
+    public void a() {
+        b();//a事务
+        c();//新事
+        int i = 10 / 0;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, timeout = 2)
+    public void b() {
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void c() {
     }
 
 }
